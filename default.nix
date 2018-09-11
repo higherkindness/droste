a5eb853fcd7539f159435fa86c549404080f55bc
Andy Scott <andy.g.scott@gmail.com>
2018-09-11 06:51:42 -0700

    Update Bazel build

--- default.nix
+++ default.nix
@@ -5,23 +5,13 @@ stdenv.mkDerivation rec {
   name = "env";
 
   src = builtins.filterSource (path: type: false) ./.;
-  
-  bazel = (import (fetchFromGitHub {
-    owner = "andyscott";
-    repo = "bazel-nix";
-    rev = "d03cebdba5aefdccad70608ec348fdd1ca0c1ed1";
-    sha256 = "08znczxmm4ijlw3lfcz8xw3f1368nn56i5ka453gygslj6qfg1g3";
-  })) {
-    version = "0.14.1";
-  };
 
   buildInputs = [
-    git sbt nodejs bazel
+    sbt bazel
   ];
 
-  installPhase = ''
-    mkdir -p $out
-    ln -s $bazel/bin/bazel $out/bazel
-  '';
+  shellHook =
+    ''
+    export IS_IN_NIX=true
+    '';
 }
-
