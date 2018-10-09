d196140bf56c29c801a3cf00f00f9297750fea4f
Christon DeWan <cmdpix@mac.com>
2018-10-08 17:38:21 -0700

    Reform DefaultTraverse (#81)
    * pulled in testcase from https://github.com/andyscott/droste/issues/76
    * fixed defaulttraverse to be (more) lawful

--- modules/core/src/main/scala/qq/droste/util/DefaultTraverse.scala
+++ modules/core/src/main/scala/qq/droste/util/DefaultTraverse.scala
@@ -8,13 +8,15 @@ import cats.arrow.Category
 import cats.data.Const
 
 import cats.instances.function._
+import cats.instances.list._
 
 trait DefaultTraverse[F[_]] extends Traverse[F] {
   override def foldMap[A, B: Monoid](fa: F[A])(f: A => B): B =
     traverse(fa)(a => Const[B, B](f(a))).getConst
 
   def foldLeft[A, B](fa: F[A], b: B)(f: (B, A) => B): B =
-    foldMap[A, B => B](fa)(a => bb => f(bb, a))(Category[Function1].algebra)(b)
+    foldMap[A,List[A]](fa)(_ :: Nil)(Monoid[List[A]])
+      .foldLeft(b)(f)
 
   def foldRight[A, B](fa: F[A], lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] =
     foldMap[A, Eval[B] => Eval[B]](fa)(a => lbb => Eval.defer(f(a, lbb)))(
