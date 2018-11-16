339ae9f701d98f5101e6ad419d29eaa89d1314bc
Pepe García <pepe@pepegar.com>
2018-11-16 14:36:04 +0100

    add more fixes to traverse derivation (#90)
    * add more fixes to traverse derivation
    
    Now @deriveTraverse:
    
    - can annotate case classes:
    
      @deriveTraverse case class Potato[A](a: A)
    
    - works when the recursive type appears deep in an AppliedTypeTree:
    
      @deriveTraverse sealed trait Adt[A]
      object Adt {
        case class ContainsLotsaPotatoes[A](potatoes: List[Potato[A]]) extends Adt[A]
      }
    
    * fix error when deriving 1lvl deep ATTs

--- modules/tests/src/test/scala/qq/droste/examples/deriveTraverse.scala
+++ modules/tests/src/test/scala/qq/droste/examples/deriveTraverse.scala
@@ -3,6 +3,7 @@ package examples
 
 import cats.instances.option._
 import cats.instances.list._
+import cats.syntax.traverse._
 
 import org.scalacheck.Properties
 import org.scalacheck.Prop._
@@ -12,11 +13,12 @@ import qq.droste.macros.deriveTraverse
 
 @deriveTraverse sealed trait ExprDerivingTraverse[A]
 object ExprDerivingTraverse {
+  @deriveTraverse final case class Box[A](name: String, a: A)
   final case class Dummy()
 
   final case class Const[A](value: BigDecimal) extends ExprDerivingTraverse[A]
-  final case class Add[A](x: A, y: A) extends ExprDerivingTraverse[A]
-  final case class AddList[A](list: List[A]) extends ExprDerivingTraverse[A]
+  final case class Add[A](x: Box[A], y: A) extends ExprDerivingTraverse[A]
+  final case class AddList[A](list: List[Box[Option[A]]]) extends ExprDerivingTraverse[A]
 }
 
 final class DeriveTraverseChecks extends Properties("deriveTraverse") {
@@ -24,8 +26,8 @@ final class DeriveTraverseChecks extends Properties("deriveTraverse") {
 
   val summingAlgebraM: AlgebraM[Option, ExprDerivingTraverse, BigDecimal] = AlgebraM {
     case Const(value) => Some(value)
-    case Add(x, y) => Some(x + y)
-    case AddList(list) => Some(list.reduce(_ + _))
+    case Add(ExprDerivingTraverse.Box(_, x), y) => Some(x + y)
+    case AddList(list) => list.map(_.a).sequence.map(_.reduce(_ + _))
   }
 
   val evaluate: Fix[ExprDerivingTraverse] => Option[BigDecimal] = scheme.cataM(summingAlgebraM)
@@ -34,13 +36,12 @@ final class DeriveTraverseChecks extends Properties("deriveTraverse") {
     evaluate(Fix(Const(1))) ?= Some(1)
 
   property("1 + 1") =
-    evaluate(Fix(Add(Fix(Const(1)), Fix(Const(1))))) ?= Some(2)
+    evaluate(Fix(Add(Box("qwer", Fix(Const(1))), Fix(Const(1))))) ?= Some(2)
 
   property("1 + 2 + 5") =
-    evaluate(Fix(Add(Fix(Add(Fix(Const(1)), Fix(Const(2)))), Fix(Const(5))))) ?= Some(8)
+    evaluate(Fix(Add(Box("qwer", Fix(Add(Box("qwer", Fix(Const(1))), Fix(Const(2))))), Fix(Const(5))))) ?= Some(8)
 
   property("1 + 2 + 3 + 4 + 5") =
-    evaluate(Fix(AddList(List(Fix(Const(1)), Fix(Const(2)), Fix(Const(3)), Fix(Const(4)), Fix(Const(5)))))) ?= Some(15)
-  
+    evaluate(Fix(AddList(List(Box("asdf", Option(Fix(Const(1)))), Box("asdf", Option(Fix(Const(2)))), Box("asdf", Option(Fix(Const(3)))), Box("asdf", Option(Fix(Const(4)))), Box("asdf", Option(Fix(Const(5)))))))) ?= Some(15)
 
 }
