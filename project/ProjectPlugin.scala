376961fa833446d77b9d61b9c45b39b05dbe121c
Juan Pedro Moreno <juanpmorest@gmail.com>
2021-07-20 19:29:25 +0200

    Renames license.txt -> COPYING to avoid conflicts

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -8,7 +8,6 @@ import sbtcrossproject.CrossProject
 import sbtcrossproject.CrossPlugin.autoImport._
 import scalajscrossproject.ScalaJSCrossPlugin.autoImport._
 import scalanativecrossproject.ScalaNativeCrossPlugin.autoImport._
-import sbtrelease.ReleasePlugin.autoImport._
 
 object ProjectPlugin extends AutoPlugin {
 
@@ -100,22 +99,10 @@ object ProjectPlugin extends AutoPlugin {
     ) ++ publishSettings
 
   lazy val publishSettings = Seq(
-    releaseCrossBuild := true,
     homepage := Some(url("https://github.com/higherkindness/droste")),
     licenses := Seq(
       "Apache 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")
     ),
-    publishMavenStyle := true,
-    Test / publishArtifact := false,
-    pomIncludeRepository := (_ => false),
-    publishTo := {
-      val nexus = "https://oss.sonatype.org/"
-      if (isSnapshot.value)
-        Some("snapshots" at nexus + "content/repositories/snapshots")
-      else
-        Some("releases" at nexus + "service/local/staging/deploy/maven2")
-    },
-    autoAPIMappings := true,
     developers := List(
       Developer(
         "andyscott",
